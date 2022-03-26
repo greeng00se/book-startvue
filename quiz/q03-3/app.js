d8d68275559e78c6373c1303a15261b8293c7950
greeng00se <thegreengoose7@gmail.com>
2022-03-26 19:37:59 +0900

    3장까지

---
+++ quiz/q03-3/app.js
@@ -0,0 +1,19 @@
+
+
+Vue.component('sibling-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>',
+})
+
+Vue.component('child-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>'
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue! passed from Parent Component',
+        anotherMessage: 'peace',
+    }
+})
