468073539161126a6c8a124c8d5b3e41e2ecdd61
greeng00se <thegreengoose7@gmail.com>
2022-05-09 23:42:55 +0900

    159 페이지까지

--- todoapp/src/components/TodoHeader.vue
+++ todoapp/src/components/TodoHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>TODO it</h1>
+        <h1>TODO it!</h1>
     </div>
 </template>
 
@@ -12,7 +12,7 @@ export default {
 
 <style scoped>
 h1 {
-    color: #23f#b52;
+    color: #2f3b52;
     font-weight: 900;
     margin: 2.5rem 0 1.5rem;
 }
